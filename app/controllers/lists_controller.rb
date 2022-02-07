490d2640241dfdb3d309aed3e1e5bfa1a4db542f
EC2 Default User <ec2-user@ip-172-31-32-72.ec2.internal>
2022-02-07 10:56:15 +0000

    Update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,29 +1,30 @@
 class ListsController < ApplicationController
   def new
-    
-    @list=List.new
-    
-  end
-  
-  def create
-  
-  list = List.new(list_params)
-  
-  list.save
-  
-  redirect_to '/top'
 
-  def index
+    @list=List.new
+
   end
+
+  def create
+  #1,&2.データを受け取り新規登録するためのインスタンス作成
+  list = List.new(list_params)
+  #3.データをデータベースに保存するためのsaveメソッド実行
+  list.save
+  #トップ画面へのリダイレクト
+
+  redirect_to '/top'
+  end
+
 
   def show
   end
 
   def edit
-  
+  end
   private
+  #ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title,body)
-    
+    params.require(:list).permit(:title,:body)
+
   end
 end

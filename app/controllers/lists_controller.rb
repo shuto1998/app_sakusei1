6fcbd92a19662cb64acff0de3b21e319e7e0cc85
EC2 Default User <ec2-user@ip-172-31-32-72.ec2.internal>
2022-02-08 13:21:13 +0000

    update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,22 +13,31 @@ class ListsController < ApplicationController
   #トップ画面へのリダイレクト
 
   redirect_to list_path(list.id)
- end
+  end
 
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+    
   end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
   private
   #ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title, :body, :image)
 
   end
 end
